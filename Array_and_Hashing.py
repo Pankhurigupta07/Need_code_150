d33c6e98a46bf5bc1c719b7064788b1cd1979e47
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-04 22:16:26 +0530

    Array and hashing

--- Array_and_Hashing.py
+++ Array_and_Hashing.py
@@ -1,3 +1,4 @@
+# brute force
 #Contains Duplicates
 # def contain_duplicate(arr,n):
 #     for i in range(n):
@@ -12,15 +13,17 @@
 # print(contain_duplicate(arr,len(arr)))
 #tc->0(n²)
 
-def contain_duplicate(arr,n):
-    set1=set()
-    for i in range(n):
-        if(arr[i] in set1):
-            return True
-        else:
-            set1.add(arr[i])
-    return False
 
-arr=[int(x) for x in input().split()]
-print(contain_duplicate(arr,len(arr)))
+#optimal approch
+# def contain_duplicate(arr,n):
+#     set1=set()
+#     for i in range(n):
+#         if(arr[i] in set1):
+#             return True
+#         else:
+#             set1.add(arr[i])
+#     return False
+
+# arr=[int(x) for x in input().split()]
+# print(contain_duplicate(arr,len(arr)))
 #tc->O(n)
