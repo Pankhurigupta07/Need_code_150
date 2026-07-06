90f531186821c12145a6fe97beed133d05648f64
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-06 13:08:13 +0530

    Array and hashing

--- Array_and_Hashing.py
+++ Array_and_Hashing.py
@@ -27,3 +27,21 @@
 # arr=[int(x) for x in input().split()]
 # print(contain_duplicate(arr,len(arr)))
 #tc->O(n)
+
+
+# valid anagram
+# brute force
+# time complexity=O(NlogN)
+# space complexity=O(N)
+
+
+def check_anagram(w1,w2):
+    if len(w1)!=len(w2):
+        return False
+
+    return "".join(sorted(w1))=="".join(sorted(w2))
+
+word1=input()
+word2=input()
+print(check_anagram(word1,word2))
+
