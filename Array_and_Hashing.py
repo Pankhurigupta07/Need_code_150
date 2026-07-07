28901596a2c00fe1a8085937369622db186dcee5
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-07 18:54:37 +0530

    Array and hashing

--- Array_and_Hashing.py
+++ Array_and_Hashing.py
@@ -177,3 +177,35 @@
 
 # time complexity=O(N)
 # space complexity=O(N)
+
+
+# Encode and Decode string
+
+def encode(strs):
+    result=""
+    for s in strs:
+        result+= str(len(s))+ "#" +s
+    return result
+
+def decode(s):
+    res=[]
+    i=0
+    while i < len(s):
+        j=i
+        while s[j]!="#":
+            j+=1
+
+        length=int(s[i:j])
+
+        word=s[j+1:j+1+length]
+        res.append(word)
+
+        i=j+1+length
+
+    return res
+
+arr=input().split()
+s=encode(arr)
+print(s)
+print(decode(s))
+
