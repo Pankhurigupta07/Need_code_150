478eade60f2563a63ab600a8c2deb81ae6537a1b
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-08 17:13:13 +0530

    Array and hashing

--- Array_and_Hashing.py
+++ Array_and_Hashing.py
@@ -253,3 +253,27 @@
 
 #time complexity=O(n²)
 #space complexity=O(n)
+
+# optimal approach
+
+def product_of_array_except_self(arr):
+    n=len(arr)
+    result=[1]*n
+
+    left_prod=1
+    for i in range(n):
+        result[i]=left_prod
+        left_prod*=arr[i]
+
+    right_prod=1
+    for i in range(n-1,-1,-1):
+        result[i]*=right_prod
+        right_prod*=arr[i]
+
+    return result
+
+arr=[int(x) for x in input().split()]
+print(product_of_array_except_self(arr))
+
+# Time complexity=O(N)
+# Space complexity=O(1)
