781ae27b0634a7611f1fa9446494c690ea52e521
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-11 17:34:13 +0530

    Two_pointer

--- Two_pointers.py
+++ Two_pointers.py
@@ -19,3 +19,30 @@
 
 # else:
 #     print("Invalid")
+
+# using two pointer
+#optimal approach
+#time complexity=O(n)
+#space complexity=O(1)
+
+
+def valid_palindrome(word):
+
+    n=len(word)
+    left=0
+    right=n-1
+
+    while left<right:
+        if word[left]!=word[right]:
+            return False
+        left+=1
+        right-=1
+    return True
+
+word=input()
+if(valid_palindrome(word)):
+    print("Valid")
+
+else:
+    print("Invalid")
+
