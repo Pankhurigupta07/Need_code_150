b66376a51c50ee7f51c9ba826c454e19a74f288e
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-12 14:38:12 +0530

    three sum

--- Two_pointers.py
+++ Two_pointers.py
@@ -64,22 +64,64 @@
 # print(two_sum(arr,target))
 
 # two pointer
-def two_sum(arr,target):
-    n=len(arr)
-    start=0
-    end=n-1
+# optimal solution
+# time complexity=O(n)
+# space complexity=O(1)
+
+# def two_sum(arr,target):
+#     n=len(arr)
+#     start=0
+#     end=n-1
     
-    while start<end:
-        if arr[start]+arr[end]==target:
-            return[start,end]
-        elif arr[start]+arr[end]<target:
-            start+=1
-        else:
-            end-=1
-    return []
+#     while start<end:
+#         if arr[start]+arr[end]==target:
+#             return[start,end]
+#         elif arr[start]+arr[end]<target:
+#             start+=1
+#         else:
+#             end-=1
+#     return []
+
+# arr=[int(x) for x in input().split()]
+# target=int(input())
+# print(two_sum(arr,target))
+
+
+# Three sum
+
+
+def three_sum(arr):
+    n=len(arr)
+    result=[]
+    arr.sort()
+
+    for i in range(n):
+
+        if i>0 and arr[i]==arr[i-1]:
+            continue
+
+        start=i+1
+        end=n-1
+
+        while start<end:
+            current_sum=arr[i]+arr[start]+arr[end]
+            if current_sum==0:
+                result.append([arr[i],arr[start],arr[end]])
+
+                start+=1
+                end-=1
+
+                while start<end  and arr[start]==arr[start-1]:
+                    start+=1
+                while start<end  and arr[end]==arr[end+1]:
+                    end-=1
+                
+            elif current_sum<0:
+                start+=1
+            else:
+                end-=1
+
+    return result
 
 arr=[int(x) for x in input().split()]
-target=int(input())
-print(two_sum(arr,target))
-
-
+print(three_sum(arr))
