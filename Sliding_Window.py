d46465dc4792b3b6f5c748f292c594693d20ecad
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-13 14:49:57 +0530

    Best time to buy and sell

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -2,21 +2,21 @@
 # optimal solution
 # time complexity=O(n)
 # space complexity=
-def buy_and_sell(arr):
-    n=len(arr)
-    buy=0
-    max_profit=0
+# def buy_and_sell(arr):
+#     n=len(arr)
+#     buy=0
+#     max_profit=0
 
-    for sell in range(1,n):
-        if arr[sell]>arr[buy]:
-            current_profit=arr[sell]-arr[buy]
-            max_profit=max(max_profit,current_profit)
+#     for sell in range(1,n):
+#         if arr[sell]>arr[buy]:
+#             current_profit=arr[sell]-arr[buy]
+#             max_profit=max(max_profit,current_profit)
 
-        else:
-            buy=sell
+#         else:
+#             buy=sell
 
 
-    return max_profit
+#     return max_profit
 
-arr=[int(x) for x in input().split()]
-print(buy_and_sell(arr))
+# arr=[int(x) for x in input().split()]
+# print(buy_and_sell(arr))
