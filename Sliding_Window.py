3b9eaccb7634e0781ee3cbfa5be18dabbcd62c6c
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-16 20:47:08 +0530

    longest repeating character replacement

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -51,24 +51,52 @@
 # time complexity=O(n)
 # space complexity=O(m)
 
-def longest_substring_without_repeating_characters(word):
+# def longest_substring_without_repeating_characters(word):
 
-    mp={}
-    l=0
-    res=0
+#     mp={}
+#     l=0
+#     res=0
 
-    for r in range(len(word)):
-        if word[r] in mp:
-            l=max(mp[word[r]]+1,l)
-        mp[word[r]]=r
-        res=max(res,r-l+1)
+#     for r in range(len(word)):
+#         if word[r] in mp:
+#             l=max(mp[word[r]]+1,l)
+#         mp[word[r]]=r
+#         res=max(res,r-l+1)
 
-    return res
+#     return res
+
+# word=input()
+# print(longest_substring_without_repeating_characters(word))
+
+# longest repeating character replacement
+# optimal approach
+# time complexity=O(n)
+# space complexity=O(1)
+
+def longest_repeating_character_replacement(word,k):
+    max_count={}
+    left=0
+    max_frequency=0
+    max_lenght=0
+
+    for right in range(len(word)):
+        max_count[word[right]]= max_count.get(word[right],0)+1
+        # if word[right] in max_count:
+            #max_count[word[right]]+=1
+        #else:
+            #max_count[word[right]]=1
+        max_frequency=max(max_frequency,max_count[word[right]])
+
+        if(right-left+1)-max_frequency>k:
+            max_count[word[left]]-=1
+            left+=1
+        max_lenght=max(max_lenght,right-left+1)
+    
+    return max_lenght
 
 word=input()
-print(longest_substring_without_repeating_characters(word))
-
-
+k=int(input())
+print(longest_repeating_character_replacement(word,k))
 
 
 
