e43e6c3795f37067f3b00aac0ee10ec218cf3a6a
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-17 21:00:29 +0530

    Valid Parentheses

---
+++ Stack.py
@@ -0,0 +1,31 @@
+# Valid Parentheses
+# optimal solution
+# time complexity=O(n)
+# space complexity=O(n)
+
+# def Valid_Parenthese(Parentheses):
+
+#     if len(Parentheses)==0:
+#         return "invalid"
+
+#     stack=[]
+#     mp={")":"(", "]":"[", "}":"{"}
+
+#     for s in Parentheses:
+#         if s in mp:
+#             top_element=stack.pop()
+
+#             if mp[s]!=top_element:
+#                  return "invalid"
+
+#         else:
+#              stack.append(s)
+        
+#     if len(stack)==0:
+#         return "valid"
+
+#     else:
+#         return "invalid"
+
+# Parentheses=input()
+# print(Valid_Parenthese(Parentheses))
