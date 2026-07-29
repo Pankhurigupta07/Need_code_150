094122f906a37b2ef8868c6a9f625406612910a9
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-29 19:34:30 +0530

    Maximum Sliding Window

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -199,6 +199,38 @@
 # print(Minimum_Window_Substring(s,t))
 
 
+# Sliding window maximum
+# Optimal Solution
+# Time complexity=O(n)
+# Stack complexity=O(n)
+
+# from collections import deque
+# def Sliding_Window_Maximum(nums,k):
+
+#     if len(nums)==0 or k==0:
+#         return []
+
+#     res=[]
+#     dq=deque()
+
+#     for i in range(len(nums)):
+#         while dq and nums[dq[-1]]< nums[i]:
+#             dq.pop()
+#         dq.append(i)
+
+#         if dq[0]<=i-k:
+#             dq.popleft()
+
+#         if i>=k-1:
+#             res.append(nums[dq[0]])
+
+#     return res
+
+# nums=[int(x) for x in input().split()]
+# k=int(input())
+# print(Sliding_Window_Maximum(nums,k))
+
+
 
 
 
