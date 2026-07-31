a7d16d92ef2bf2a8d201c9172949be99372030c8
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-07-31 17:59:05 +0530

    Search  in rotated sorted array uding optimal approach

--- Binary_Search.py
+++ Binary_Search.py
@@ -149,3 +149,37 @@
 # print(Findminnum(nums))
 
 
+# Search in rotated sorted array
+# optimal solution
+# tc=O(logn)
+# sc=O(1)
+
+# def findTarget(nums,target):
+
+#     low=0
+#     high=len(nums)-1
+
+#     while low<=high:
+#         mid=(low+high)//2
+
+#         if nums[mid]==target:
+#             return True
+        
+#         if nums[low]<=nums[mid]:
+#             if nums[low]<=target<nums[mid]:
+#                 high=mid-1
+#             else:
+#                 low=mid+1
+
+#         else:
+
+#             if nums[mid]<target<=nums[high]:
+#                 low=mid+1
+#             else:
+#                 high=mid-1
+
+#     return False
+
+# nums=[int(x) for x in input().split()]
+# target=int(input())
+# print(findTarget(nums,target))
