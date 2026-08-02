0f7db52b5085c1dafbcd135e2afba39cf529d839
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-08-02 15:02:21 +0530

    Reverse Linked List using optimal approach

--- Linked_List.py
+++ Linked_List.py
@@ -29,3 +29,46 @@
 # Node2.next=Node3
 
 # ll.displayLinkedlist()
+
+# Reverse Linked List
+# optimal solution
+# Time complexity=O(n)
+# Space complexity=O(1)
+
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
+
+# def reverse_linked_list(head):
+#     prev=None
+#     curr=head
+
+#     while curr:
+#         nxt=curr.next
+#         curr.next=prev
+#         prev=curr
+#         curr=nxt
+
+#     return prev
+
+# def display_Linked_List(head):
+#     temp=head
+#     while temp:
+#         print(temp.data,end="->")
+#         temp=temp.next
+
+#     print("None")
+    
+
+# Node1=Node(10)
+# Node2=Node(20)
+# Node3=Node(30)
+
+# Node1.next=Node2
+# Node2.next=Node3
+
+# display_Linked_List(Node1)
+
+# reverse_ll=reverse_linked_list(Node1)
+# display_Linked_List(reverse_ll)
