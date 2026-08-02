ba14f794ef8b88360369e22a78f0711cc044d914
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-08-02 14:08:54 +0530

    Creating and printing linked list

--- Linked_List.py
+++ Linked_List.py
@@ -1,4 +1,31 @@
-# Reverse Linked List
-# optimal solution
+# Creating and printing Linked List
+
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
+
+# class Linkedlist:
+#     def __init__(self):
+#         self.head=Node
+
+#     def displayLinkedlist(self):
+#         curr=self.head
+#         while curr:
+#             print(curr.data,end="->")
+#             curr=curr.next
+
+#         print("None")
 
 
+# ll=Linkedlist()
+
+# Node1=Node(10)
+# Node2=Node(20)
+# Node3=Node(30)
+
+# ll.head=Node1
+# ll.head.next=Node2
+# Node2.next=Node3
+
+# ll.displayLinkedlist()
