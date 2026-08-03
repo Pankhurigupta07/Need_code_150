02545cc7218fdee4a6641d94c8b6e9499a577aeb
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-08-03 19:42:16 +0530

    Merge two sorted list

--- Linked_List.py
+++ Linked_List.py
@@ -74,4 +74,63 @@
 # display_Linked_List(reverse_ll)
 
 
+# Merge two sorted list
+# optimal solution
 
+# class ListNode:
+#     def __init__(self,val=0,next=None):
+
+#         self.val=val
+#         self.next=next
+
+# def creating_linked_list(arr):
+#     head=ListNode(int(arr[0]))
+#     curr=head
+#     for val in arr[1:]:
+#         curr.next=ListNode(int(val))
+#         curr=curr.next
+#     return head
+
+# def Merge_Linked_List(list1,list2):
+
+#     dummy=ListNode(0)
+#     tail=dummy
+
+#     while  list1 and list2:
+#         if list1.val<=list2.val:
+#             tail.next=list1
+#             list1=list1.next
+
+#         else:
+#             tail.next=list2
+#             list2=list2.next
+        
+#         tail=tail.next
+
+#     tail.next= list1 if list1 else list2
+#     return dummy.next
+
+# def display_Linked_List(head):
+
+#     curr=head
+#     while curr:
+#         print(curr.val,end="->")
+#         curr=curr.next
+
+#     print("None")
+
+
+# l1_input=["1","3","8"]
+# l2_input=["4","7","12","17"]
+
+# ll1=creating_linked_list(l1_input)
+# ll2=creating_linked_list(l2_input)
+
+# merge=Merge_Linked_List(ll1,ll2)
+
+# print("Merge List:")
+# display_Linked_List(merge)
+
+
+
+    
